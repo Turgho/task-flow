8d6efe7e2ad1cef9eac77abddba77edae7bc65df
Turgho <victor.hugo3692111@gmail.com>
2025-03-26 13:43:30 -0300

    add: users service

---
+++ src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';;
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>
+  ) {}
+  
+  async findById(id: string): Promise<User|null>{
+    const user = this.usersRepository.findOne({ where: { id } })
+    return user;
+  }
+}
