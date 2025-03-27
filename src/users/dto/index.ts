7c91ee4aea4454c1557bb45cd35af643a290cfd6
Turgho <victor.hugo3692111@gmail.com>
2025-03-27 20:27:31 -0300

    add: index user dto

---
+++ src/users/dto/index.ts
@@ -0,0 +1,5 @@
+// Export all DTOs grouped by type
+export * from '../dto/create-user.dto';
+export * from '../dto/delete-user.dto';
+export * from '../dto/find-user.dto';
+export * from '../dto/update-user.dto';
