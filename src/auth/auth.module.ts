a9c976333b1ce07825c74c8c57b4705f55534fad
Turgho <victor.hugo3692111@gmail.com>
2025-03-27 20:22:08 -0300

    add: jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { HashModule } from './hash/hash.module';
+import { PasswordHasherModule } from '../users/services/passwordHasher/passwordHasher.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtAuthGuard } from './jwt/jwt.guard';
 
 @Module({
-  imports: [HashModule],
-  providers: [],
-  exports: [],
+  imports: [
+    PasswordHasherModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    })
+  ],
+  providers: [JwtAuthGuard],
+  exports: [JwtModule],
 })
 export class AuthModule {}
