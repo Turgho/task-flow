b21bce96a455fe9d252c44ee869a673129c3ae19
Turgho <victor.hugo3692111@gmail.com>
2025-03-27 20:26:34 -0300

    add: index task use case

---
+++ src/tasks/use-case/index.ts
@@ -0,0 +1,5 @@
+// Export all task class UseCases
+export * from './create-task.use-case';
+export * from './update-task.use-case';
+export * from './delete-task.use-case';
+export * from './find-task.use-case';
